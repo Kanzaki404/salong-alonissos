98a5b887e4ba9842e07262c54013971552dd1c5e
Alexander <Alexander.risteski23@gmail.com>
2020-06-07 16:49:55 +0200

    navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import React from 'react';
-import './App.scss';
+import React from "react";
+import "./App.scss";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Nav from './components/Nav'
+import Footer from './components/Footer'
+import LandingPage from './pages/LandingPage'
+import Services from './pages/Services'
+import Prices from './pages/Prices'
+import OpeningHours from './pages/OpeningHours'
+import About from './pages/About'
+
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <div className="App">
+        <Nav/>
+        <Switch>
+                <Route path = '/' exact component={LandingPage} />
+                <Route path = '/services' component={Services} />
+                <Route path = '/prices' component={Prices} />
+                <Route path = '/hours' component={OpeningHours} />
+                <Route path = '/about' component={About} />
+        </Switch>
+        <Footer/>
+
+      </div>
+    </Router>
   );
 }
 
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 export default function Nav() {
     return (
-        <div>
-            
-        </div>
+        <nav>
+            <ul>
+                <Link to='/'>
+                    <li>Home</li>
+                </Link>
+                <Link to='/services'>
+                    <li>Services</li>
+                </Link>
+                <Link to='/prices'>
+                    <li>Prices</li>
+                </Link>
+                <Link to='/hours'>
+                    <li>Opening hours</li>
+                </Link>
+                <Link to='/about'>
+                    <li>About</li>
+                </Link>          
+            </ul>
+        </nav>
     )
 }
