0aba4062421bb9a154954eaf0561f351004356fe
Alexander <Alexander.risteski23@gmail.com>
2020-06-07 16:29:45 +0200

    Barebones2

---
+++ src/pages/OpeningHours.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function OpeningHours() {
+    return (
+        <div>
+            <h1>OpeningHours</h1>
+        </div>
+    )
+}
---
+++ src/pages/Prices.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Prices() {
+    return (
+        <div>
+            <h1>Prices</h1>
+        </div>
+    )
+}
